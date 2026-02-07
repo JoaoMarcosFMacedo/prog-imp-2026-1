8efade6692ae58eee5bc27a168e6ba8648a2a19e
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-02-07 00:25:13 +0000

    Criei um Scanner

--- Exec01.java
+++ Exec01.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Exec01{
+    public static Scanner scanner = new Scanner(System.in);
     public static void main( String [] args){
         System.out.println("Óla, professor!");
+        int idade;
+        idade = scanner.nextInt();
+        System.out.println(idade);
+        scanner.nextInt(); //para receber o entrer do número;
+        String nome = scanner.nextLine();
     }
+
 }
