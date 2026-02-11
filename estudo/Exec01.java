3391f4091bae7e9a7f3951e5b76cb4c388827980
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-02-11 00:23:11 +0000

    Código com Scanner e  System.out.printf

--- estudo/Exec01.java
+++ estudo/Exec01.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 public class Exec01{
     public static Scanner scanner = new Scanner(System.in);
     public static void main( String [] args){
-        System.out.println("Óla, professor!");
+
         int idade;
+
+        System.out.println("Óla, professor!");
+        System.out.print("Digite sua idade:");
         idade = scanner.nextInt();
-        System.out.println(idade);
-        scanner.nextInt(); //para receber o entrer do número;
+        scanner.nextLine(); //para receber o entrer do número;
+        System.out.printf("Sua idade é %d ", idade);
+        System.out.println();       
+        System.out.print("e Qual o seu nome? ");
         String nome = scanner.nextLine();
+
+        System.out.printf("%s , que nome bonito!", nome);
+
+        
     }
 
 }
