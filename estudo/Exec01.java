3a34938f758d04e09c7b45a01546bb81d1c8f417
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-02-25 00:39:22 +0000

    em casa termnino

--- estudo/Exec01.java
+++ estudo/Exec01.java
@@ -4,46 +4,73 @@ public class Exec01{
     public static final int ANO_FINAL = 2026;
     public static void main( String [] args){
 
-        int idade;
+        int idade = 0, qtsPessoa, somaIdade = 0;
+        double media;
+        
+        // System.out.println("Óla, professor!");
+        // System.out.print("Digite sua idade:");
 
-        System.out.println("Óla, professor!");
-        System.out.print("Digite sua idade:");
+        // idade = scanner.nextInt();
 
-        idade = scanner.nextInt();
+        // scanner.nextLine(); //para receber o entrer do número;
 
-        scanner.nextLine(); //para receber o entrer do número;
+        // System.out.printf("Sua idade é %d ", idade);
 
-        System.out.printf("Sua idade é %d ", idade);
+        // System.out.println();
 
-        System.out.println();
+        // System.out.print("e Qual o seu nome? ");
+        // String nome = scanner.nextLine();
 
-        System.out.print("e Qual o seu nome? ");
-        String nome = scanner.nextLine();
+        // System.out.printf("%s , que nome bonito!", nome);
+        // System.out.println();
 
-        System.out.printf("%s , que nome bonito!", nome);
-        System.out.println();
+        // if( idade >= 18 ){
+        //     System.out.printf("Sua idade : %d", idade);
+        // }
+        
+        // else{
+        //     System.out.printf("Sua idade: %d", idade);
+        // }
+        // System.out.println();
 
-        if( idade >= 18 ){
-            System.out.printf("Sua idade : %d", idade);
+        // if (idade < 16 ){
+        //     System.out.printf("Idade: %d - Você não é obrigatorio votar", idade);
+        // } else if( idade < 18 ){
+        //     System.out.printf("Idade: %d - Seu voto é facultativo.", idade);
+        // } else if( idade < 60 ){
+        //     System.out.printf("Idade %d -  Seu voto é obrigatorio", idade);
+        // } else {
+        //     System.out.printf("Idade %d - Seu não é mais necessário votar", idade);
+        // }
+        // System.out.println();
+        // int anoNasc = ANO_FINAL - idade;
+        // System.out.printf("Você provavelmente nasceu em %d ", idade);
+
+        // for(int i = 0; i < 15; i ++){
+        //     System.out.printf("%d", i);
+        // }
+        
+        for(int i = 1; i>=qtsPessoa; i+= 1){
+            // qtsPessoa +=1;
+            somaIdade += idade;
+
+            System.out.println("Digite sua idade");
+            idade = scanner.nextInt();
+            System.out.println();
+            System.out.printf("Quantidade de pessoas: %d° - Soma da idade: %d", i, somaIdade);
+
         }
         
-        else{
-            System.out.printf("Sua idade: %d", idade);
-        }
-        System.out.println();
+        if(qtsPessoa >=1){
 
-        if (idade < 16 ){
-            System.out.printf("Idade: %d - Você não é obrigatorio votar", idade);
-        } else if( idade < 18 ){
-            System.out.printf("Idade: %d - Seu voto é facultativo.", idade);
-        } else if( idade < 60 ){
-            System.out.printf("Idade %d -  Seu voto é obrigatorio", idade);
-        } else {
-            System.out.printf("Idade %d - Seu não é mais necessário votar", idade);
+            media = somaIdade / qtsPessoa ;
+            System.out.printf(" A média é %f" , media);
+            System.out.println();
         }
-        System.out.println();
-        int anoNasc = ANO_FINAL - idade;
-        System.out.printf("Você provavelmente nasceu em %d ", idade);
+        else {
+            System.out.println("Nenhuma pessoa foi cadastrada");
+        }
+
     }
 
 }
