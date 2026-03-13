b7000ec3c63022e1487a8350397601bb4a58f053
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-03-13 23:16:53 +0000

    FAlta terminar

--- Atividade/Exec01.java
+++ Atividade/Exec01.java
@@ -2,29 +2,46 @@ import java.util.Scanner;
 
 public class Exec01 {
 
-    public static Scanner scanner = new Scanner(System.in);
     public static final int TAM = 10;
-    public static void main(String [] args){
-        int[] numerosSemRept = new int[TAM];
-        int N;
-        PreencherVetor(numerosSemRept);
-        imprimir(numerosSemRept, N);
+    public static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int[] numeros = new int[TAM];
+
+        preencher(numeros);
+        imprimir(numeros);
     }
-    public static void PreencherVetor(int[] v){
-        for(int i = 0 ; i >= v.length; i +=1 ){
-            System.out.printf("Seu vetor está na posição %d", i);
-            v[1] = scanner.nextInt();
+
+    public static void preencher(int[] v) {
+        for (int i = 0; i < v.length; i += 1) {
+
+            System.out.print("Digite um número: ");
+            int valor = scanner.nextInt();
+
+            if (existe(v, valor, i)) {
+                System.out.println("Valor repetido! Recomece a digitação.");
+
+                i = -1; 
+                continue;
+            }
+
+            v[i] = valor;
         }
     }
-    public static void imprimir(int[] v, int TAM) {
-        System.out.print("{");
-            if (TAM > 0) {
-        System.out.print(" " + v[0]);
+
+    public static boolean existe(int[] v, int valor, int limite) {
+        for (int i = 0; i < limite; i += 1) {
+            if (v[i] == valor) {
+                return true;
+            }
         }
-        for (int i = 1; i < TAM; i += 1) {
-        System.out.print(", " + v[i]);
+        return false;
     }
-        System.out.println(" }");
-  }
+
+    public static void imprimir(int[] v) {
+        for (int i = 0; i < v.length; i += 1) {
+            System.out.print(v[i] + " ");
+        }
+        System.out.println();
+    }
 }
