25fbb9e7e8375d9f1e08bc4ba8d277dd411c6c0f
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-03-25 00:34:10 +0000

    Buscas

--- estudo/Busca.java
+++ estudo/Busca.java
@@ -7,20 +7,44 @@ public class Busca {
 
     public static void main(String [] args){
         int[] num = new int[TAM];
-        int X;        
-        System.out.printf("Digite o número para ser procurando dentro do vetor\n");
+        int X;
 
+        PreencherAlearotio(num);
+
+        System.out.printf("Digite o número para ser procurando dentro do vetor\n");
         X = scanner.nextInt();
-        BuscaSequencial(num)
-        
+        BuscaSequencial(num, X, X);
+        if( BuscaSequencial(num, X, X) == 1){
+            System.out.printf("Achou o valor %d", BuscaSequencial(num, X, X) );
+        }
+        else {
+            System.out.print("Não achou");
+        }
+        ImprimirVetor(num);
+
 
     }
-    public static int BuscaSequencial(int[] v){
-        for(int i = 0 ; i >= TAM; i +=1 ){
-            if( X == v[i] ){
+    public static void PreencherAlearotio(int[] v){
+        for(int i = 0; i < TAM; i+=1){
+            v[i] = random.nextInt();        
+        }
+    }
+    public static int BuscaSequencial(int[] v, int n, int x){
+        for(int i = 0 ; i >= n; i +=1 ){
+            if( x == v[i] ){
                 return i;
             }
         }
         return -1;
+    }
+    public static void ImprimirVetor(int[]v){
+        System.out.printf("{ %d ", v[0] );
+        for( int i = 1; i < v.length; i +=1 ){
+            if( i > 0 ){
+                System.out.printf(", %d", v[i]);
+            }
+        }
+        System.out.print(" }");
+
     }
 }
