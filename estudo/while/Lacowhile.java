46d03b3822ae4247fd6e5c98e1b3c7befffb6d1b
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-04-10 23:03:43 +0000

    .

---
+++ estudo/while/Lacowhile.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Lacowhile{
+    
+    public static Scanner scanner = new Scanner(System.in);
+    public static final int idade = 0;
+    public static void main( String []args){
+        int B = 5;
+        lacowhile();
+
+
+        Rlacowhile(B);
+    }
+
+    public static void lacowhile( ){
+        System.out.print("Digite um valor para x: ");
+        double x = scanner.nextDouble();
+        
+        while( x <= idade ){
+
+                System.out.print("Idade invalida, digite novamente: ");
+                x = scanner.nextDouble();
+            }
+            System.out.println("Maior que 0");
+        
+    }
+
+
+
+    public static int Rlacowhile( int B ){
+        int c;
+        System.out.println("Digite o valor para C");
+        c = scanner.nextInt();
+
+        while( c != idade){
+
+            System.out.println("tente dnv c");
+            c = scanner.nextInt();
+
+            
+        }
+        return c;
+    }
+}
