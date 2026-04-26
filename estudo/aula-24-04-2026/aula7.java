5fdca25d4c8f92673a996f63bb36be887bff09a7
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-04-26 20:41:54 +0000

    Matriz

--- estudo/aula-24-04-2026/aula7.java
+++ estudo/aula-24-04-2026/aula7.java
@@ -4,13 +4,16 @@ public class aula7{
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String [] args){
         int[][] m = new int [4][3];
+        int[][] sm = new int [4][3];
+
 
         System.out.println("teste");
         preencherMatriz(m);
         
-        imprimirVetor(m);
+        imprimirMatriz(m);
     
     }
+    
     
     public static void preencherMatriz(int[][]m ){
 
@@ -18,7 +21,7 @@ public class aula7{
 
             for( int j = 0; j < m[0].length; j+= 1){
 
-                System.out.printf("Digite o valor para a matriz [%d] x [%d]", i, j);
+                System.out.printf("Digite o valor para a matriz [%d] x [%d]: ", i, j);
 
                 m[i][j] = scanner.nextInt();
                     // scanner.next();
@@ -28,13 +31,15 @@ public class aula7{
             System.out.printf("\n");
         }
     }
-    public static void imprimirVetor(int[][] m){
-        for(int i = 0; i < m.length ; i+=1){
-            for(int j = 0; j< m[0].length ; j += 1){
+    public static void imprimirMatriz(int[][] m){
+        for(int i = 0; i < m.length ; i += 1){
+            System.out.printf("{ ");
+            for(int j = 0; j < m[0].length ; j += 1){
 
-                System.out.printf(" { %d } { %d }\n",m[i], m[j]);
+                System.out.printf(" %d  ", m[i][j]);
+
             }
-            System.out.printf("\n");
+            System.out.printf(" }\n");    
         }
     }
 }
